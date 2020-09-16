d56344be6d9c65a91a466a874f1a7e6ef636d903
AchmadZahrulAZ <achmadzahrul01@gmail.com>
2020-09-16 21:07:45 +0700

    test upload github

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main {
+
+	printf("Nama		: Achmad Zahrul Ali Zadan");
+	printf("NPM		: 19081010081");
+	printf("Jurusan		: Teknik Informatika");
+	printf("Angkatan	: 2019");
+
+	return 0;
+}
